c1df76c8ae656a1d062ece35d55563039cdb7bf3
Bala Paranj <bparanj@onekingslane.com>
2016-05-25 15:59:23 -0700

    stock ticker using actioncontroller live streaming

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,24 @@
+require 'ticker/sse'
+
 class WelcomeController < ApplicationController
+  include ActionController::Live
+  
   def index
-    
+  end
+  
+  def ticker
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = Ticker::SSE.new(response.stream)
+    begin
+      loop do
+        quotes = [410, 411, 415, 413, 414, 420]
+        sse.write({ SRV:  quotes.sample})
+        sleep 2
+      end
+    rescue IOError
+      logger.info 'Client disconnects causes IOError on write'
+    ensure
+      sse.close
+    end
   end
 end
